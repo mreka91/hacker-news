07632d3d256018a5f52fd7d141c4d07f3bc3de2a
Reka Madarasz <mreka91@gmail.com>
2020-12-17 11:15:40 +0100

    success message

--- app/users/update.php
+++ app/users/update.php
@@ -25,6 +25,11 @@ if (isset($_POST['bio'], $_POST['email'], $_POST['password'])) {
 
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
+    //show a success message and redirect back to the profile page
+    if ($user) {
+        successMessage("Profile successfully updated!");
+        redirect('../../profile.php');
+    }
 
     // get the user info and save it to Session
     $statementInfo = $database->prepare('SELECT bio, email, password FROM users WHERE id = :id');
@@ -36,10 +41,5 @@ if (isset($_POST['bio'], $_POST['email'], $_POST['password'])) {
     $_SESSION['user']['bio'] = $userInfo['bio'];
     $_SESSION['user']['email'] = $userInfo['email'];
     $_SESSION['user']['password'] = $userInfo['password'];
-
-    //show a success message and redirect back to the profile page
-    if ($userInfo) {
-        successMessage("Profile successfully updated!");
-        redirect('/profile.php');
-    }
 }
+redirect('/');
