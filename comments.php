96bb84df71bf1328feb04828dac55629ed742089
Reka Madarasz <mreka91@gmail.com>
2020-12-22 10:32:46 +0100

    started comments front

--- comments.php
+++ comments.php
@@ -1,22 +1,37 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
+<?php
+$id = $_GET['id'];
+$statement = $database->prepare('SELECT * FROM posts  WHERE id = :id ');
+$statement->bindParam(':id', $id, PDO::PARAM_INT);
+$statement->execute();
+
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+?>
+
 <article>
+    <h2><?= $post['title'] ?></h2>
+    <p> <a href="<?= $post['post_link'] ?>"><?= $post['post_link'] ?> </a></p>
+    <p><?= $post['description'] ?></p>
+    <small>Posted at <?= $post['created_at'] ?></small>
+</article>
+<article>
+    <?php if (isset($_SESSION['user'])) : ?>
+        <form class="" action="addcomment.php" method="post">
+            <hr>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" value="<?= $_SESSION['user']['name'] ?>" class="form-control" required>
+            </div><!-- /form-group -->
 
-    <form class="col-lg-6 offset-lg-3" action="" method="post">
-        <hr>
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" class="form-control">
-        </div><!-- /form-group -->
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea name="message" id="message" class="form-control" required></textarea>
+            </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="message">Message</label>
-            <textarea name="message" id="message" class="form-control"></textarea>
-        </div><!-- /form-group -->
-
-        <button type="submit" class="btn btn-primary">Add comment</button>
-    </form><!-- /col-lg-6 -->
-
+            <button type="submit" class="btn btn-primary">Add comment</button>
+        </form><!-- /col-lg-6 -->
+    <?php endif; ?>
 
     <div class="row mt-4">
         <div class="col-lg-6 offset-lg-3">
--- posts.php
+++ posts.php
@@ -1,7 +1,7 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php
 
-$statement = $database->prepare('SELECT posts.title, posts.post_link, posts.description, posts.created_at, users.name FROM posts INNER JOIN users ON users.id = posts.user_id ORDER BY created_at DESC ');
+$statement = $database->prepare('SELECT posts.id, posts.title, posts.post_link, posts.description, posts.created_at, users.name FROM posts INNER JOIN users ON users.id = posts.user_id ORDER BY created_at DESC ');
 $statement->execute();
 
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -9,17 +9,18 @@ $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <article>
     <h1>Newest posts</h1>
-    <p>Here you can read the newest posts.</p>
+    <p> Read what's new on Hacker News!</p>
 
     <?php foreach ($posts as $post) : ?>
-        <h2><?= $post['title'] ?></h2>
-        <p> <a href="<?= $post['post_link'] ?>"><?= $post['post_link'] ?> </a></p>
-        <p><?= $post['description'] ?></p>
-        <p>Posted at <?= $post['created_at'] ?></p>
-        <p>By <?= $post['name'] ?></p>
-        <a href="">Comments</a>
+        <article>
+            <h2><?= $post['title'] ?></h2>
+            <p> <a href="<?= $post['post_link'] ?>"><?= $post['post_link'] ?> </a></p>
+            <p><?= $post['description'] ?></p>
+            <small>Posted at <?= $post['created_at'] ?></small>
+            <small>By <?= $post['name'] ?></small>
+            <a href="comments.php?id=<?= $post['id']; ?>">Comments</a>
+        </article>
     <?php endforeach; ?>
-
 </article>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
